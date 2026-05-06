3553f58f3662a73772aaac6ca8c5c5d3729d79dc
Gideon van den Herik <g.vandenherik@gmsnl.com>
2026-05-06 16:40:56 +0200

    Initial commit + Project Start

---
+++ Views/UserControls/MenuBar.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace GidEditor.Views.UserControls;
+
+public partial class MenuBar : UserControl
+{
+    public MenuBar()
+    {
+        InitializeComponent();
+    }
+}
